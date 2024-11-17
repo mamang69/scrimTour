a616c7089587b1b1036b998ab9ce2c0b497a3f0a
mamang69 <elditamvan15@gmail.com>
2024-11-17 17:08:05 +0700

    Add login and register

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,22 +1,25 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     {{-- @vite('resources/css/app.css') --}}
-    <title>Scrim Tour</title> 
-  </head>
-  <body>
+    <title>Scrim Tour</title>
+</head>
+
+<body>
     @include('partial.navbar')
-    
+
     @yield('heroimage')
     <div class="container">
-      @yield('container')
+        @yield('container')
     </div>
 
     @include('partial.footer')
     <script src="{{ mix('js/app.js') }}"></script>
-  </body>
+</body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,20 @@ module.exports = {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
-      ],
+    ],
     theme: {
         extend: {},
+        container: {
+            center: true, // Memastikan container selalu di tengah
+            padding: {
+                DEFAULT: "1rem", // Padding default untuk semua layar
+                sm: "2rem", // Padding khusus untuk layar kecil
+                lg: "4rem", // Padding khusus untuk layar besar
+                xl: "5rem", // Padding khusus untuk layar ekstra besar
+            },
+        },
     },
-    plugins: [],
+    plugins: [
+        require("@tailwindcss/forms"), // Tambahkan plugin ini
+    ],
 };
