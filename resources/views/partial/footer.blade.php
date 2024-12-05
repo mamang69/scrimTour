542e2db85493cd6bfc9152baeacf329788f33cc4
FaisalIbnulHakim <203040027@mail.unpas.ac.id>
2024-12-05 14:10:33 +0700

    memperbaiki navbar dan footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,27 +1,27 @@
-<footer class="bg-gray-800 text-white py-8">
+<footer class="bg-red-700 text-black py-8">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <!-- Baris pertama: Jadwal Game Article -->
-          <div class=" text-center">
-            <h3 class="text-lg font-semibold">
-              <a href="/jadwal" class="mx-2">Jadwal</a>
-              <a href="/game" class="mx-2">Game</a>
-              <a href="/article" class="mx-2">Article</a>
-            </h3>
-            <h3 class="text-lg font-semibold ">
-              Hubungi Kami
-            </h3>
-            <div class="mt-4">
-              <a href="https://wa.me/6281234567890" target="_blank" class="text-white hover:text-green-500">
-                <i class="fab fa-whatsapp text-3xl"></i>
-                <h3>Whatsapp</h3>
-              </a>
-            </div>
-          </div>
-
+      <div class="text-center">
+        <h3 class="text-lg font-semibold">
+          <a href="/jadwal" class="mx-5 font-bold">Jadwal</a>
+          <a href="/game" class="mx-5 font-bold">Game</a>
+          <a href="/article" class="mx-5 font-bold">Artikel</a>
+          <a href="" class="mx-5 font-bold">Tentang Kami</a>
+        </h3>
+        <h3 class="text-lg font-bold mt-6 ">
+          Hubungi Kami
+        </h3>
+        <div class="mt-4">
+          <a href="https://wa.me/6281234567890" target="_blank" class="text-black font-bold inline-flex items-center justify-center space-x-2">
+            <i class="fab fa-whatsapp text-3xl"></i>
+            <span class="text-2xl">Whatsapp</span>
+          </a>
+        </div>
+      </div>
 
       <!-- Garis pemisah -->
-      <div class="border-t border-gray-700 mt-8 pt-6 text-center">
-          <p class="text-sm text-gray-500">&copy; 2024 Your Company. All rights reserved.</p>
+      <div class="border-t-2 border-black mt-8 pt-6 text-center">
+          <p class="font-bold text-black">&copy; Copyright 2024. All Right Reserved.</p>
       </div>
   </div>
 </footer>
